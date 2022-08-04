ea98e7e1f65fb78d9d0c9969754baa173b446906
ProCodeRed <yadav.vipinkumar@outlook.com>
2022-08-04 14:41:39 +0530

    added anchor tag to Link using react router dom

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
     <div className='about_wrapper'>
     <h1>This is About page</h1>
-    <a href="/">Go back</a>
+    <Link to="/">Go back</Link>
     </div>
   )
 }
